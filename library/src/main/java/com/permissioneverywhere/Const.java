a6bdfa3f1f86228dbcf87d086abb173779498a68
Your Name <you@example.com>
2016-04-30 12:11:42 +0500

    init

--- library/src/main/java/com/permissioneverywhere/Const.java
+++ library/src/main/java/com/permissioneverywhere/Const.java
@@ -6,5 +6,7 @@ package com.permissioneverywhere;
 public class Const {
     public static final String PERMISSIONS_ARRAY = "permissions_array";
     public static final String REQUEST_CODE = "request_code";
+    public static final String RESULT_RECEIVER = "result_receiver";
     public static final int DEFAULT_CODE = 123;
+    public static final String GRANT_RESULT = "grant_result";
 }
--- library/src/main/java/com/permissioneverywhere/PermissionActivity.java
+++ library/src/main/java/com/permissioneverywhere/PermissionActivity.java
@@ -2,21 +2,28 @@ package com.permissioneverywhere;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.os.ResultReceiver;
+
+import java.util.Arrays;
 
 /**
  * Created by Farruxx on 30.04.2016.
  */
 public class PermissionActivity extends Activity {
-    private int requestCode;
+
+    private ResultReceiver resultReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         if(getIntent() != null) {
+
+            resultReceiver = getIntent().getParcelableExtra(Const.RESULT_RECEIVER);
             String[] permissionsArray = getIntent().getStringArrayExtra(Const.PERMISSIONS_ARRAY);
-            requestCode = getIntent().getIntExtra(Const.REQUEST_CODE, Const.DEFAULT_CODE);
+            int requestCode = getIntent().getIntExtra(Const.REQUEST_CODE, Const.DEFAULT_CODE);
             ActivityCompat.requestPermissions(this, permissionsArray, requestCode);
         }else {
             finish();
@@ -26,9 +33,11 @@ public class PermissionActivity extends Activity {
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if(this.requestCode == requestCode){
-
-        }
+        Bundle bundle = new Bundle();
+        bundle.putStringArray(Const.PERMISSIONS_ARRAY, permissions);
+        bundle.putIntArray(Const.GRANT_RESULT, grantResults);
+        resultReceiver.send(requestCode, bundle);
+        finish();
     }
+
 }
