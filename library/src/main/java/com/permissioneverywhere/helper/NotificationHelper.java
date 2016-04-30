09cd960e8532bf6a529d9bf6ac6bf3fae60d54e3
Your Name <you@example.com>
2016-04-30 10:49:34 +0500

    init

--- library/src/main/java/com/permissioneverywhere/helper/NotificationHelper.java
+++ library/src/main/java/com/permissioneverywhere/helper/NotificationHelper.java
@@ -7,7 +7,8 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
-import com.permissioneverywhere.MainActivity;
+
+import com.permissioneverywhere.PermissionActivity;
 import com.permissioneverywhere.R;
 
 public class NotificationHelper {
@@ -16,7 +17,7 @@ public class NotificationHelper {
   public static final int REQUEST_CODE_PUSH = 77;
 
   private void sendNotification(Context context,String message, String type) {
-    Intent intent = new Intent(context, MainActivity.class);
+    Intent intent = new Intent(context, PermissionActivity.class);
     intent.putExtra(KEY_TYPE, type);
     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
     PendingIntent pendingIntent = PendingIntent.getActivity(context, REQUEST_CODE_PUSH, intent,
@@ -24,7 +25,7 @@ public class NotificationHelper {
 
     Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
     NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context)
-        .setSmallIcon(R.mipmap.ic_launcher)
+//        .setSmallIcon(R.mipmap.ic_launcher)TODO FIX app data
         .setContentTitle(context.getString(R.string.app_name))
         .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
         .setContentText(message)
