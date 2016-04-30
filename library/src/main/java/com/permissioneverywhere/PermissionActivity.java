663987e71fc1ec599cccc615685866e15b4a3134
Your Name <you@example.com>
2016-04-30 23:37:07 +0500

    init

--- library/src/main/java/com/permissioneverywhere/PermissionActivity.java
+++ library/src/main/java/com/permissioneverywhere/PermissionActivity.java
@@ -1,12 +1,11 @@
 package com.permissioneverywhere;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.os.ResultReceiver;
-
-import java.util.Arrays;
 
 /**
  * Created by Farruxx on 30.04.2016.
@@ -20,24 +19,44 @@ public class PermissionActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         if(getIntent() != null) {
-
             resultReceiver = getIntent().getParcelableExtra(Const.RESULT_RECEIVER);
             String[] permissionsArray = getIntent().getStringArrayExtra(Const.PERMISSIONS_ARRAY);
             int requestCode = getIntent().getIntExtra(Const.REQUEST_CODE, Const.DEFAULT_CODE);
-            ActivityCompat.requestPermissions(this, permissionsArray, requestCode);
+            if(!hasPermissions(permissionsArray)) {
+                ActivityCompat.requestPermissions(this, permissionsArray, requestCode);
+            }else {
+                onComplete(requestCode, permissionsArray, new int[]{PackageManager.PERMISSION_GRANTED});
+            }
         }else {
             finish();
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    private void onComplete(int requestCode, String[] permissions, int[] grantResults) {
         Bundle bundle = new Bundle();
         bundle.putStringArray(Const.PERMISSIONS_ARRAY, permissions);
         bundle.putIntArray(Const.GRANT_RESULT, grantResults);
+        bundle.putInt(Const.REQUEST_CODE, requestCode);
         resultReceiver.send(requestCode, bundle);
         finish();
+
+    }
+
+    private boolean hasPermissions(String[] permissionsArray) {
+        boolean result = true;
+        for(String permission:permissionsArray) {
+            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        onComplete(requestCode, permissions, grantResults);
     }
 
 }
