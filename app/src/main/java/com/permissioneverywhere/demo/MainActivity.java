55a067f8590a2dbe7855f839a822cc2ec23aef70
Your Name <you@example.com>
2016-04-30 10:50:53 +0500

    init

--- app/src/main/java/com/permissioneverywhere/demo/MainActivity.java
+++ app/src/main/java/com/permissioneverywhere/demo/MainActivity.java
@@ -1,16 +1,19 @@
-package com.permissioneverywhere;
+package com.permissioneverywhere.demo;
 
 import android.Manifest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.permissioneverywhere.Const;
+import com.permissioneverywhere.PermissionActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(com.permissioneverywhere.R.layout.activity_main);
 
         Intent intent = new Intent(getApplicationContext(), PermissionActivity.class);
         intent.putExtra(Const.REQUEST_CODE,234);
