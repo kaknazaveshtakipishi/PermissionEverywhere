c7529693d8f7d31c9530d0203ceca15850b5c1bd
Eugene Maksymenko <maxigene@gmail.com>
2018-12-06 19:57:57 +0200

    Fix notification appearance in Android O.

--- library/src/main/java/com/permissioneverywhere/NotificationHelper.java
+++ library/src/main/java/com/permissioneverywhere/NotificationHelper.java
@@ -1,52 +1,66 @@
 package com.permissioneverywhere;
 
-import android.Manifest;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.os.ResultReceiver;
 
+class NotificationHelper {
+    public static final int REQUEST_CODE_PUSH = 77;
 
- class NotificationHelper {
-  public static final int REQUEST_CODE_PUSH = 77;
+    private static final String CHANNEL_ID = "permission";
 
-  public static void sendNotification(Context context,
-                                      String[] permissions,
-                                      int requestCode,
-                                      String notificationTitle,
-                                      String notificationText,
-                                      int notificationIcon,
-                                      ResultReceiver receiver) {
+    public static void sendNotification(Context context,
+                                        String[] permissions,
+                                        int requestCode,
+                                        String notificationTitle,
+                                        String notificationText,
+                                        int notificationIcon,
+                                        ResultReceiver receiver) {
 
-    Intent intent = new Intent(context, PermissionActivity.class);
-    intent.putExtra(Const.REQUEST_CODE, requestCode);
-    intent.putExtra(Const.PERMISSIONS_ARRAY, permissions);
-    intent.putExtra(Const.RESULT_RECEIVER, receiver);
-    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+        Intent intent = new Intent(context, PermissionActivity.class);
+        intent.putExtra(Const.REQUEST_CODE, requestCode);
+        intent.putExtra(Const.PERMISSIONS_ARRAY, permissions);
+        intent.putExtra(Const.RESULT_RECEIVER, receiver);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+        }
 
-    PendingIntent pendingIntent = PendingIntent.getActivity(context, REQUEST_CODE_PUSH, intent,
-        PendingIntent.FLAG_ONE_SHOT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, REQUEST_CODE_PUSH, intent,
+                PendingIntent.FLAG_ONE_SHOT);
 
-    Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-    NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context)
-        .setSmallIcon(notificationIcon)
-        .setContentTitle(notificationTitle)
-        .setStyle(new NotificationCompat.BigTextStyle().bigText(notificationText))
-        .setContentText(notificationText)
-        .setAutoCancel(true)
-        .setSound(defaultSoundUri)
-        .setPriority(Notification.PRIORITY_HIGH)
-        .setVibrate(new long[0])
-        .setContentIntent(pendingIntent);
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-    NotificationManager notificationManager =
-        (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, context.getString(R.string.channel_name), NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setShowBadge(true);
+            notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
 
-    notificationManager.notify(requestCode, notificationBuilder.build());
-  }
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(notificationIcon)
+                .setContentTitle(notificationTitle)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(notificationText))
+                .setContentText(notificationText)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setVibrate(new long[0])
+                .setContentIntent(pendingIntent);
+
+        notificationManager.notify(requestCode, notificationBuilder.build());
+    }
 }
